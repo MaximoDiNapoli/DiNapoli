9ac254bfb56c16e0fc419be8aa9cd6eb12d979dd
MaximoDiNapoli <63366993+MaximoDiNapoli@users.noreply.github.com>
2020-08-15 04:28:35 -0300

    Add files via upload
    Ahora si esta completamente funcional aunque todavía le faltan unos retoques en la ultima funcion que actualizare en el fin de semana

--- Veterinaria/src/Administacion/Main.java
+++ Veterinaria/src/Administacion/Main.java
@@ -4,8 +4,7 @@ import Animales.Animal;
 import Animales.Perro;
 import Animales.Gato;
 import Animales.Tortuga;
-
-
+import javax.swing.*;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 import java.time.Instant;
@@ -18,8 +17,6 @@ import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
-//Todavia le faltan unos detalles pero muy poquito ahora todo como esta funciona pero tengo que hacer que pueda recibir cualquier cosa(perro, tortuga, gato)
-//y funcione, ahora solo funciona con el perro agus pero tengo planes de terminar de mejorarlo en la brevedad
 public class Main {
 
     public static String verQueTipoDeAnimalTuvoMejorAtencionYearActual(Perro Agus, Gato Hongui, Tortuga Pancha){
@@ -60,7 +57,7 @@ public class Main {
         }
         return "empate";
     }
-    public static boolean verVisitasYearActualPorRaza(Perro Agus){
+    public static void verVisitasYearActualPorRaza(ArrayList<Perro> perros){
         SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
         int dea = 0;
         Date inicioYear = new Date();
@@ -70,7 +67,19 @@ public class Main {
         catch (ParseException e){
             e.printStackTrace();
         }
-        for (int i = 0; i < Agus.getFechasAtencion().size(); i++){
+        ArrayList<Date> temp = new ArrayList<>();
+        for (int i = 0; i < perros.size(); i++) {
+            temp = perros.get(i).getFechasAtencion();
+            for (int j = 0; j < temp.size(); j++) {
+                if(temp.get(j).after(inicioYear)){
+                    dea = dea + 1;
+                }
+            }
+            if(dea < 4)System.out.println("La raza: "+perros.get(i).getRazaPerro()+ "  no cumple con las visitas");
+            dea = 0;
+        }
+    }
+        /*for (int i = 0; i < Agus.getFechasAtencion().size(); i++){
             if(Agus.getFechasAtencion().get(i).after(inicioYear)){
                 dea = dea + 1;
             }
@@ -79,10 +88,8 @@ public class Main {
             System.out.println("La raza: "+Agus.getRazaPerro()+ " Cumple con las visitas");
             return true;
         }
-        System.out.println("La raza: "+Agus.getRazaPerro()+ "no Cumple con las visitas");
-        return false;
+        System.out.println("La raza: "+Agus.getRazaPerro()+ "no Cumple con las visitas");*/
 
-    }
     public static Date sumarDiasAFecha(Date inicioYearVar, int dias){
         if (dias==0) return inicioYearVar;
         Calendar calendar = Calendar.getInstance();
@@ -91,26 +98,29 @@ public class Main {
         return calendar.getTime();
     }
 
-    public static boolean verVisitasDesdeQueNacio(Perro Agus){
+    public static boolean verVisitasDesdeQueNacio(Animal animalDesignado, int opcion){
         SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
         int dea = 0;
         int dias;
         Date inicioYearVar = new Date();
         try {
-            inicioYearVar = Agus.getFechaDeNacimiento();
+            inicioYearVar = animalDesignado.getFechaDeNacimiento();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < Agus.getFechasAtencion().size(); i++) {
+        for (int i = 0; i < animalDesignado.getFechasAtencion().size(); i++) {
             inicioYearVar = sumarDiasAFecha(inicioYearVar, 365);
-            for (int j = 0; j < Agus.getFechasAtencion().size(); j++){
-                if(Agus.getFechasAtencion().get(j).before(inicioYearVar)){
+            for (int j = 0; j < animalDesignado.getFechasAtencion().size(); j++){
+                if(animalDesignado.getFechasAtencion().get(j).before(inicioYearVar)){
                     dea = dea + 1;
                 }
             }
-            if(dea < 4) {System.out.println("No");return false;}
+            if(dea < 4 && opcion == 1) {System.out.println("No");return false;}
+            if(dea < 2 && opcion == 2) {System.out.println("No");return false;}
+            if(dea < 1 && opcion == 3) {System.out.println("No");return false;}
             dea = 0;
         }
+
         System.out.println("Si");
         return true;
 
@@ -156,17 +166,21 @@ public class Main {
         }
         return false;
     }
-    public static void main(String[] argum){
+    public static void main(String[] argum) {
         Main main = new Main();
         ArrayList<Date> fechasAtencionAgus = new ArrayList<>();
+        ArrayList<Date> fechasAtencionChicho = new ArrayList<>();
         ArrayList<Date> fechasAtencionHongui = new ArrayList<>();
         ArrayList<Date> fechasAtencionPancha = new ArrayList<>();
+        ArrayList<Perro> perros = new ArrayList<>();
         SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
         Date fechaNacimientoAgus = new Date();
+        Date fechaNacimientoChicho = new Date();
         Date fechaNacimientoHongui = new Date();
         Date fechaNacimientoPancha = new Date();
         try {
             fechaNacimientoAgus = ft.parse("2019-03-10");
+            fechaNacimientoChicho = ft.parse("2019-03-10");
             fechaNacimientoHongui = ft.parse("2018-03-10");
             fechaNacimientoPancha = ft.parse("2008-03-10");
             fechasAtencionAgus.add(ft.parse("2020-03-10"));
@@ -177,6 +191,8 @@ public class Main {
             fechasAtencionAgus.add(ft.parse("2019-04-10"));
             fechasAtencionAgus.add(ft.parse("2020-06-10"));
             fechasAtencionAgus.add(ft.parse("2020-07-10"));
+
+            fechasAtencionChicho.add(ft.parse("2020-03-10"));
 
             fechasAtencionHongui.add(ft.parse("2019-03-10"));
             fechasAtencionHongui.add(ft.parse("2018-03-10"));
@@ -193,24 +209,26 @@ public class Main {
             fechasAtencionPancha.add(ft.parse("2013-01-10"));
             fechasAtencionPancha.add(ft.parse("2014-01-10"));
             fechasAtencionPancha.add(ft.parse("2015-01-10"));
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
-            catch (ParseException e){
-                e.printStackTrace();
-        }
-        Perro Agus = new Perro("Agus",fechaNacimientoAgus,1,fechasAtencionAgus,"Bugeldog");
-        Gato Hongui = new Gato("Hongui",fechaNacimientoHongui,2,fechasAtencionHongui, "Mestizo");
-        Tortuga Pancha = new Tortuga("Pancha",fechaNacimientoPancha,3,fechasAtencionPancha, true);
-        Animal animalDesignado = Agus;
+        Perro agus = new Perro("agus", fechaNacimientoAgus, 1, fechasAtencionAgus, "Bugeldog");
+        Perro chicho = new Perro("chicho", fechaNacimientoAgus, 4, fechasAtencionChicho, "salchicha");
+        Gato hongui = new Gato("hongui", fechaNacimientoHongui, 2, fechasAtencionHongui, "Mestizo");
+        Tortuga pancha = new Tortuga("pancha", fechaNacimientoPancha, 3, fechasAtencionPancha, true);
+        perros.add(agus);
+        perros.add(chicho);
         System.out.println("Que animal es? " +
                 "1:Perro      " +
                 "2:Gato       " +
                 "3:Tortuga    ");
         Scanner leer = new Scanner(System.in);
         int opcion = leer.nextInt();
+        Animal animalDesignado = chicho;
         verVisitasYearActual(animalDesignado, opcion);
-        verVisitasDesdeQueNacio(Agus);
-        verVisitasYearActualPorRaza(Agus);
-        System.out.println(verQueTipoDeAnimalTuvoMejorAtencionYearActual(Agus, Hongui, Pancha));
+        verVisitasDesdeQueNacio(animalDesignado, opcion);
+        verVisitasYearActualPorRaza(perros);
+        System.out.println(verQueTipoDeAnimalTuvoMejorAtencionYearActual(agus, hongui, pancha));
     }
 
 }
